b7db18250708dfec28a9ba65500fef5ef5a3ce28
Katrinka Swenson <kjswenso@gmail.com>
2019-11-22 14:30:23 -0500

    Enabled CORS Cross Origin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,57 @@ import './App.css';
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            meals : []
-        }
-    }
+    state = {
+        isLoading: true,
+        meals: [],
+        users: [],
+    };
 
     componentDidMount() {
+        Promise.all([fetch('http://localhost:8080/api/meals'), fetch('http://localhost:8080/api/user')])
+            .then(([res1, res2]) => {
+                console.log(res1, res2);
+                return Promise.all([res1.json(), res2.json()])
+            })
+            .then(([res1, res2]) => {
+                this.setState({
+                    meals: res1,
+                    users: res2,
+                })
+            })
     }
 
-    getMeals() {
-        fetch('http://localhost:8080/api/meals')
-            .then(res = > res.json())
-            .then((data) => {
-            this.setState( {meals: data } )
-        })
-    }
+    // componentDidMount() {
+    //     Promise.all([fetch('/api/meals'), fetch('/api/user')])
+    //
+    //         .then(([res1, res2]) => {
+    //             return Promise.all([res1.json(), res2.json()])
+    //         })
+    //         .then(([res1, res2]) => {
+    //             this.setState({
+    //                     meals: res1,
+    //                     users: res2,
+    //
+    //         });
+    // }
 
+    // componentDidMount() {
+    //     this.getMeals();
+    // }
+    //
+    // getMeals() {
+    //     fetch('/api/meals')
+    //         .then(response => response.json())
+    //         .then(data => this.setState({meals: data, isLoading: false}));
+    // }
 
     render() {
+
+        const {meals, isLoading} = this.state;
+
+        if(isLoading) {
+            return <p>Loading...</p>
+        }
         return (
             <header>
                 <h1>Planily</h1>
@@ -32,9 +63,12 @@ class App extends Component {
                     <button>Home</button>
                     <button>Calendar</button>
                 </nav>
-                <ul>
-                    <li>{this.state.meals}</li>
-                </ul>
+                <h2>Meals</h2>
+                {meals.map(meal =>
+                <div key={meal.id}>
+                    {meal.name}
+                </div>
+                )}
             </header>
     );
     }
